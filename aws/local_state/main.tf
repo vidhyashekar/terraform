3a9c9144a69c6f9ed1a2d7a1e4552e88513b382e
Vidhya Shekar SJ <vidhyashekarsj@Vidhyas-MacBook-Pro.local>
2026-02-20 14:50:07 +0530

    Add AWS provider

---
+++ aws/local_state/main.tf
@@ -0,0 +1,32 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical (Ubuntu)
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-*-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_instance" "app_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "terraform-demo"
+  }
+}
+
+# Create a VPC
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+}
